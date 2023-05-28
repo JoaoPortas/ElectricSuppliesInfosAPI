1eb64361f5ff0cab050f588dd6c00b17f5ed9646
João Portas <portas.joao.developer@gmail.com>
2023-05-28 18:55:52 +0100

    New routes management

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, jsonify, abort
 from managers.SeleniumManager import SeleniumManager
 from scrapers.selenium_scraper import SeleniumScraper
-from scrapers.legrand_scraper import LegrandScraper
-
-selenium_manager = SeleniumManager()
-selenium_manager.setup_driver()
+from routes.legrand_routes import legrand_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(legrand_bp, url_prefix="/legrand")
 
 
 @app.route("/")
@@ -20,60 +19,6 @@ def hello_world():
         page: A page with hello world!
     """
     return "<h1>Hello, World!</h1>"
-
-
-@app.route("/selenium")
-def selenium():
-    """
-    Tests the selenium in the app.
-
-    To see if selenium is well configuration accessible by /selenium
-
-    Returns:
-        page: With the selenium scrapped data.
-    """
-    selenium_scraper = SeleniumScraper(selenium_manager.get_driver())
-    result = selenium_scraper.scrape_website()
-    # selenium_manager.close_driver()
-    return f"<h1>Scraped title: {result}</h1>"
-
-
-@app.route("/legrand/<product_id>")
-def legrand(product_id):
-    """
-    Route to scap information about the products in the Legrand Website.
-
-    Go to /legrand/<id> to get the information about a product, where the <id> is the ID of the internal
-    reference of the product in the store.
-
-    Args:
-        product_id: Internal ID of the product in the store to search for.
-
-    Returns:
-        JSON with the information of the product if was found.
-        If the product was not found returns a simple page.
-    """
-
-    reset_data_source = request.args.get("resetDataSource", default="False")
-
-    if reset_data_source.lower() == "true":
-        reset_data_source = True
-    else:
-        reset_data_source = False
-
-    print(reset_data_source)
-
-    legrand_scraper = LegrandScraper(selenium_manager.get_driver())
-
-    print(f"Getting Legrand product info for ID {product_id} ...")
-    result = legrand_scraper.scrape_product_info(product_id, reset_data_source)
-
-    if result is not None:
-        print(f"Legrand product with ID {product_id} found")
-        return result.to_json()
-    else:
-        print(f"Legrand product with ID {product_id} not found!")
-        abort(404, "Product not found")
 
 
 @app.errorhandler(404)
