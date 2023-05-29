674d6b64157cb31bc56dc85f147f9a3a02c09a88
João Portas <portas.joao.developer@gmail.com>
2023-05-30 00:03:05 +0100

    Efapel products scraping

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.error_handlers import error_handlers_bp
 from routes.legrand_routes import legrand_bp
+from routes.efapel_routes import efapel_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(error_handlers_bp)
 app.register_blueprint(legrand_bp, url_prefix="/legrand")
+app.register_blueprint(efapel_bp, url_prefix="/efapel")
 
 
 @app.route("/")
